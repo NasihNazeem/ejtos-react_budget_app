a537824d98b0fdd6176c9136581204843f989f2d
NasihNazeem <nasihnazeem@gmail.com>
2023-12-19 01:52:07 -0500

    done until task 3

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,11 +2,23 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const totalExpenses = expenses.map(obj => obj.cost).reduce((accumulator, currentValue) => accumulator + currentValue, 0);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        if (event.target.value < 20000 && event.target.value > totalExpenses){
+            setNewBudget(event.target.value);
+            console.log(event.target.value)
+        } else {
+            if(event.target.value > 20000){
+                alert("The value cannot exceed £20000.")
+            } else {
+                alert(`You cannot reduce the budget value below your spending (${totalExpenses}).`)
+            }
+        }
+
     }
+
     return (
 <div className='alert alert-secondary'>
 <span>Budget: £{budget}</span>
