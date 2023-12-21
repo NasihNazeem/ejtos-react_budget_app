a30db92cf6a2be3d08dcda8ba9db71a0b6786696
NasihNazeem <nasihnazeem@gmail.com>
2023-12-20 20:48:13 -0700

    feat: project complete

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,29 +1,40 @@
-import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Budget = () => {
-    const { budget, expenses } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const totalExpenses = expenses.map(obj => obj.cost).reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    const handleBudgetChange = (event) => {
-        if (event.target.value < 20000 && event.target.value > totalExpenses){
-            setNewBudget(event.target.value);
-            console.log(event.target.value)
-        } else {
-            if(event.target.value > 20000){
-                alert("The value cannot exceed £20000.")
-            } else {
-                alert(`You cannot reduce the budget value below your spending (${totalExpenses}).`)
-            }
-        }
-
+  const { budget, expenses, currency } = useContext(AppContext);
+  const [newBudget, setNewBudget] = useState(budget);
+  const totalExpenses = expenses
+    .map((obj) => obj.cost)
+    .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  const handleBudgetChange = (event) => {
+    if (event.target.value < 20000 && event.target.value > totalExpenses) {
+      setNewBudget(event.target.value);
+      console.log(event.target.value);
+    } else {
+      if (event.target.value > 20000) {
+        alert("The value cannot exceed £20000.");
+      } else {
+        alert(
+          `You cannot reduce the budget value below your spending (${totalExpenses}).`
+        );
+      }
     }
+  };
 
-    return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
-    );
+  return (
+    <div className="alert alert-secondary">
+      <span>
+        Budget: {currency}
+        {budget}
+      </span>
+      <input
+        type="number"
+        step="10"
+        value={newBudget}
+        onChange={handleBudgetChange}
+      ></input>
+    </div>
+  );
 };
 export default Budget;
